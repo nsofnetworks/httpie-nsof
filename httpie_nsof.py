nsofnetworks/httpie-nsof

commit f82220729b1b6bbde76f38c2fe8b79e18a48ea0c

httpie_nsof: Adjusted httpie-nsof to use new login API

httpie_nsof.py
@@ -13,9 +13,14 @@ import jwt
 import sys
 
 
-__version__ = '0.9'
+__version__ = '0.95'
 __author__ = 'Alon Horowitz'
 __licence__ = 'Apache 2.0'
+
+# endpoints
+EP_LOGIN = '/v1/login/auth'
+EP_GET_AUTHCODE = '/v1/login/authorize'
+EP_GET_TOKEN = '/v1/oauth/token'
 
 
 class NsofAuth(object):
@@ -25,56 +30,73 @@ class NsofAuth(object):
         self.password = password
         self.eorg = os.getenv('EORG', org)
         self.verbose = bool(os.getenv('VERBOSE', False))
+        self.host_url = None
 
     def __call__(self, r):
-        host_url = self._get_host_url(r)
-        access_token = self._load_token_if_valid('access_token', host_url)
+        self.host_url = self._get_host_url(r)
+        access_token = self._load_token_if_valid('access_token')
         if not access_token:
-            tokens = self._refresh_authentication(host_url)
+            tokens = self._refresh_authentication()
             if not tokens:
-                tokens = self._authenticate(host_url)
+                tokens = self._authenticate()
             if tokens:
                 access_token = tokens['access_token']
                 self._store_token('access_token',
-                                  host_url,
                                   access_token,
                                   store_eorg=True)
                 if 'refresh_token' in tokens:
                     self._store_token('refresh_token',
-                                      host_url,
                                       tokens['refresh_token'])
         if access_token:
             r.headers['Authorization'] = 'Bearer %s' % access_token
         return r
 
-    def _refresh_authentication(self, host_url):
-        refresh_token = self._load_token_if_valid('refresh_token', host_url)
+    def _refresh_authentication(self):
+        refresh_token = self._load_token_if_valid('refresh_token')
         if not refresh_token:
             return None
-        request_data = {"grant_type": "refresh_token",
-                        "refresh_token": refresh_token}
-        return self._request_token(host_url, request_data)
+        json = {"grant_type": "refresh_token",
+                "refresh_token": refresh_token}
+        if self.eorg != self.org:
+            json['scope'] = 'org:%s' % self.eorg
+        return self._do_call(EP_GET_TOKEN, 'post', json=json)
 
-    def _authenticate(self, host_url):
-        request_data = {"grant_type": "password",
-                        "username": self.username,
-                        "password": self.password}
-        return self._request_token(host_url, request_data)
+    def _authenticate(self):
+        json = {"username": self.username,
+                "password": self.password,
+                "org_shortname": self.org}
+        response = self._do_call(EP_LOGIN, 'post', json=json)
+        if response['status'] != 'authorized':
+            raise Exception('Could not log in. Returned status: %s',
+                            response['status'])
 
-    def _is_auth_endpoint_exists(self, host_url):
-        url = self._get_auth_url(host_url)
+        params = {'response_type': 'code',
+                  'client_id': 'api',
+                  'session_token': response['session_token']}
+        response = self._do_call(EP_GET_AUTHCODE, 'get', params=params)
+
+        json = {'grant_type': 'authorization_code',
+                'client_id': 'api',
+                'code': response['code']}
+        if self.eorg != self.org:
+            json['scope'] = 'org:%s' % self.eorg
+        return self._do_call(EP_GET_TOKEN, 'post', json=json)
+
+    def _is_auth_endpoint_exists(self, url):
         response = requests.options(url=url)
         return response.status_code == 200
 
-    def _request_token(self, host_url, request_data):
-        if not self._is_auth_endpoint_exists(host_url):
+    def _do_call(self, ep, method, params=None, json=None):
+        url = self.host_url + ep
+        if not self._is_auth_endpoint_exists(url):
             return None
-        if self.eorg != self.org:
-            request_data['scope'] = "org:%s" % self.eorg
-        url = self._get_auth_url(host_url)
-        msg = "httpie-nsof: [%s] %s body=%s" % (self.eorg, url, request_data)
+        msg = "httpie-nsof: [%s] url=%s" % (self.eorg, url)
+        if params:
+            msg += ", params=%s" % params
+        if json:
+            msg += ", body=%s" % json
         self._vprint(msg)
-        response = requests.post(url=url, json=request_data)
+        response = requests.request(method, url, params=params, json=json)
         response.raise_for_status()
         return response.json()
 
@@ -85,17 +107,14 @@ class NsofAuth(object):
             return "%s://%s" % (parsed_url.scheme, netloc_no_port)
         return netloc_no_port
 
-    def _get_auth_url(self, host_url):
-        return "%s/v1/%s/oauth/token" % (host_url, self.org)
-
-    def _load_token_if_valid(self, name, host_url):
+    def _load_token_if_valid(self, name):
         path = self._get_token_path(name)
         try:
             with open(path) as f:
                 token_info = json.load(f)
         except:
             return None
-        if token_info['host_url'] != host_url:
+        if token_info['host_url'] != self.host_url:
             return None
         if token_info.get('eorg', self.eorg) != self.eorg:
             return None
@@ -104,9 +123,9 @@ class NsofAuth(object):
             return None
         return token_info['token']
 
-    def _store_token(self, name, host_url, token, store_eorg=False):
+    def _store_token(self, name, token, store_eorg=False):
         path = self._get_token_path(name)
-        token_info = {'token': token, 'host_url': host_url}
+        token_info = {'token': token, 'host_url': self.host_url}
         if store_eorg:
             token_info['eorg'] = self.eorg
         with open(path, 'w') as f:

commit 5fa2e24d4a516d79db34dc2a4ee9ec29c8cb7968

httpie_nsof: fix check for auth endpoint existence
The goal of this feature is to automatically skip auth when calling non
nsof endpoints with this plugin. This will remove the need to choose a
different auth plugin. This commit fixes the functionality broken by
commit "f822207 httpie_nsof: Adjusted httpie-nsof to use new login API".

httpie_nsof.py
@@ -67,6 +67,8 @@ class NsofAuth(object):
         return self._do_call(EP_GET_TOKEN, 'post', json=json)
 
     def _authenticate(self):
+        if not self._is_auth_endpoint_exists():
+            return None
         if self.username.startswith("key-") and '@' not in self.username:
             return self._authenticate_api_key()
         else:
@@ -101,15 +103,13 @@ class NsofAuth(object):
             json['scope'] = 'org:%s' % self.eorg
         return self._do_call(EP_GET_TOKEN, 'post', json=json)
 
-    def _is_auth_endpoint_exists(self, url):
+    def _is_auth_endpoint_exists(self):
+        url = self.host_url + EP_GET_TOKEN
         response = requests.options(url=url)
         return response.status_code == 200
 
     def _do_call(self, ep, method, params=None, json=None):
         url = self.host_url + ep
-        if not self._is_auth_endpoint_exists(url):
-            print("target endpoint failed to respond (URL: %s)." % url)
-            exit(1)
         msg = "httpie-nsof: [%s] url=%s" % (self.eorg, url)
         if params:
             msg += ", params=%s" % params